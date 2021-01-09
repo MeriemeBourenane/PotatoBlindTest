63e6e15de07f72369b1c651bce3b81d2dcae6e1e
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-09 19:19:15 +0100

    Add statsGame

---
+++ src/io/potatoBlindTest/gameEngine/statsGame/StatesGame.java
@@ -0,0 +1,18 @@
+package io.potatoBlindTest.gameEngine.statsGame;
+
+public enum StatesGame {
+
+    CREATED(900),
+    INIT(901),
+    SATARTED(902),
+    ENDED(903);
+
+    private int value;
+    private StatesGame(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
