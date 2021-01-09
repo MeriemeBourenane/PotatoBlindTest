4da46cbac2fb662fe3cddbcdf068a3c6bace7bf3
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-09 19:30:16 +0100

    Add following features to the Network Module:
    - JoinAsPlayer
    - verify his name and modify it or not

--- src/io/potatoBlindTest/network/handlerMessage/serverNetwork/clientMessageTypeHandlers/game/JoinAsPlayer.java
+++ src/io/potatoBlindTest/network/handlerMessage/serverNetwork/clientMessageTypeHandlers/game/JoinAsPlayer.java
@@ -1,21 +1,65 @@
 package io.potatoBlindTest.network.handlerMessage.serverNetwork.clientMessageTypeHandlers.game;
 
 import io.potatoBlindTest.gameEngine.NamePlayer;
+import io.potatoBlindTest.gameEngine.Player;
+import io.potatoBlindTest.gameEngine.statsGame.StatesGame;
 import io.potatoBlindTest.network.ClientHandler;
+import io.potatoBlindTest.network.ServerGame;
 import io.potatoBlindTest.network.communication.Message;
+import io.potatoBlindTest.network.communication.MessageAttachment;
+import io.potatoBlindTest.network.handlerMessage.clientNetwork.serverTypesMessages.ServerMessageType;
 import io.potatoBlindTest.network.handlerMessage.serverNetwork.ClientTypesMessages.ClientMessageHandler;
+import io.potatoBlindTest.network.handlerMessage.serverNetwork.notification.SubjectClientHandler;
 
-public class JoinAsPlayer implements ClientMessageHandler<NamePlayer> {
+import java.util.Random;
+
+public class JoinAsPlayer extends SubjectClientHandler implements ClientMessageHandler<NamePlayer> {
 
     // The current clientHandler containes all clientHandlers of the ServerNetwork and all the ServerGames
     @Override
     public Message handle(NamePlayer dataMessage, ClientHandler clientHandlers) {
         System.out.println("Handling join as player ...");
         // Join as player :
-        // - add it to the CopyOnWriteArrayList<ServerGame>
-        // - send the new ServerSocket 's ServerGame to the client
+        // - add it and his socket to the mapPlayerSocket if the state game is INIT && name of player not already given
+        // - answer to the client with name unchanged or changed
+        // - notify the creator with the NamePlayer
         Message messageToSend;
 
-        return null;
+        if (clientHandlers.getServerNetwork().isServerGame() && ((ServerGame)clientHandlers.getServerNetwork()).getStatesGame() == StatesGame.INIT) {
+
+            // while the name of the player is already given
+            Player player = new Player(dataMessage.getName());
+            while (((ServerGame)clientHandlers.getServerNetwork()).getMapPlayerClientHandler().containsKey(player)) {
+                Random rand= new Random();
+                player.setName(dataMessage.getName() + "-"+ rand.nextInt(10_000));
+            }
+
+            ((ServerGame)clientHandlers.getServerNetwork()).getMapPlayerClientHandler().put(player, clientHandlers);
+
+            messageToSend = new Message(ServerMessageType.OK.getValue());
+            System.out.println("[JOinAsPlayer] code : " + messageToSend.getCode());
+
+        } else {
+            messageToSend = new Message(ServerMessageType.FORBIDDEN.getValue());
+        }
+
+        if (messageToSend.getCode() == ServerMessageType.OK.getValue()) {
+            Thread threadNotify = new Thread(() -> {
+
+                NamePlayer namePlayer = new NamePlayer(dataMessage.getName());
+                Message notifiation = new MessageAttachment<NamePlayer>(ServerMessageType.NEW_PLAYER_IN_GAME.getValue(), namePlayer);
+                try {
+                    this.notifyOne(((ServerGame)clientHandlers.getServerNetwork()).getMapPlayerClientHandler()
+                                    .get(((ServerGame)clientHandlers.getServerNetwork()).getCreator()),
+                            notifiation);
+                } catch (Exception e) {
+                    System.out.println("[JoinAsPlayer] Can't notify the creator ...");
+                }
+
+            });
+            threadNotify.start();
+        }
+
+        return messageToSend;
     }
 }
