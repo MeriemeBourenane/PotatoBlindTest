e6340a819386e9fded81c51643f05c36aa298401
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-09 16:46:14 +0100

    Add following features to the Network Game Module :
    - hashMap of player, socket
    - a Player creator

--- src/io/potatoBlindTest/network/ServerGame.java
+++ src/io/potatoBlindTest/network/ServerGame.java
@@ -4,24 +4,29 @@ import io.potatoBlindTest.gameEngine.Player;
 import io.potatoBlindTest.gameEngine.Turn;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ServerGame extends ServerNetwork{
 
-    private CopyOnWriteArrayList<Player> players;
+    private ConcurrentHashMap<Player, Socket> mapPlayerSocket;
+    private Player creator;
     private Turn currentTurn;
 
     public ServerGame(ServerSocket serverSocket) throws IOException {
         super(serverSocket);
-        this.players = new CopyOnWriteArrayList<>();
+        this.mapPlayerSocket = new ConcurrentHashMap<>();
+        this.creator = null;
         this.currentTurn = null;
     }
 
-    public CopyOnWriteArrayList<Player> getPlayers() {
-        return players;
+    public ConcurrentHashMap<Player, Socket> getMapPlayerSocket() {
+        return mapPlayerSocket;
     }
 
     public Turn getCurrentTurn() {
@@ -31,4 +36,18 @@ public class ServerGame extends ServerNetwork{
     public void setCurrentTurn(Turn currentTurn) {
         this.currentTurn = currentTurn;
     }
+
+    @Override
+    public boolean isServerGame() {
+        return true;
+    }
+
+    public Player getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Player creator) {
+        this.creator = creator;
+    }
+
 }
