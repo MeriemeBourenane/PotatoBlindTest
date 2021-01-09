91f8280a93e0bcc9f9b207ec38aff1ae24588ced
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-09 23:24:09 +0100

    Add a GameEngine attribut and remove the Turn in ServerGame

--- src/io/potatoBlindTest/network/ServerGame.java
+++ src/io/potatoBlindTest/network/ServerGame.java
@@ -1,16 +1,11 @@
 package io.potatoBlindTest.network;
 
-import io.potatoBlindTest.gameEngine.Game;
+import io.potatoBlindTest.gameEngine.GameEngine;
 import io.potatoBlindTest.gameEngine.Player;
-import io.potatoBlindTest.gameEngine.Turn;
 import io.potatoBlindTest.gameEngine.statsGame.StatesGame;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -19,30 +14,21 @@ public class ServerGame extends ServerNetwork {
 
     private ConcurrentHashMap<Player, ClientHandler> mapPlayerClientHandler;
     private Player creator;
-    private Turn currentTurn;
     private StatesGame statesGame;
     private AtomicInteger nbReadyPlayer;
+    private GameEngine gameEngine;
 
     public ServerGame(ServerSocket serverSocket, CopyOnWriteArrayList<ServerGame> serverGames) throws IOException {
         super(serverSocket);
         this.mapPlayerClientHandler = new ConcurrentHashMap<>();
         this.creator = null;
-        this.currentTurn = null;
         this.nbReadyPlayer = new AtomicInteger(0);
+        this.gameEngine = new GameEngine();
     }
 
     public ConcurrentHashMap<Player, ClientHandler> getMapPlayerClientHandler() {
         return mapPlayerClientHandler;
     }
-
-    public Turn getCurrentTurn() {
-        return currentTurn;
-    }
-
-    public void setCurrentTurn(Turn currentTurn) {
-        this.currentTurn = currentTurn;
-    }
-
     @Override
     public boolean isServerGame() {
         return true;
