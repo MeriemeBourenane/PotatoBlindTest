b1b51411d35fc3839f14d5969e1e00d933ba2afe
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-09 21:19:03 +0100

    Wait to receive the beginning message in waitingStart

--- src/io/potatoBlindTest/controller/WaitingStartController.java
+++ src/io/potatoBlindTest/controller/WaitingStartController.java
@@ -1,6 +1,10 @@
 package io.potatoBlindTest.controller;
 
+import io.potatoBlindTest.gameEngine.NamePlayer;
 import io.potatoBlindTest.network.communication.Message;
+import io.potatoBlindTest.network.communication.MessageAttachment;
+import io.potatoBlindTest.network.handlerMessage.clientNetwork.serverTypesMessages.ServerMessageType;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -27,6 +31,16 @@ public class WaitingStartController implements UIController {
     @Override
     public void handleMessage(Message incomingMessage) {
         // TODO: handle Start game messages
+        switch (ServerMessageType.valueOfLabel(incomingMessage.getCode())) {
+            case BEGINNING_GAME: ;
+                Platform.runLater(() -> {
+                    ControllerClient.initializeReadyView(playerNameLabel.getText(), null);
+                });
+                break;
+            default:
+                System.out.println("Unwanted message");
+                break;
+        }
     }
 
     @FXML
