14c0ee490bcb28cced8f07f4a9525b8f68559532
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-10 01:52:11 +0100

    Implement SubmitAnswer handler

--- src/io/potatoBlindTest/network/handlerMessage/serverNetwork/clientMessageTypeHandlers/turn/SubmitAnswer.java
+++ src/io/potatoBlindTest/network/handlerMessage/serverNetwork/clientMessageTypeHandlers/turn/SubmitAnswer.java
@@ -1,14 +1,18 @@
 package io.potatoBlindTest.network.handlerMessage.serverNetwork.clientMessageTypeHandlers.turn;
 
-import io.potatoBlindTest.gameEngine.Answer;
-import io.potatoBlindTest.gameEngine.NamePlayer;
+import io.potatoBlindTest.gameEngine.*;
+import io.potatoBlindTest.gameEngine.statsGame.StatesGame;
 import io.potatoBlindTest.network.ClientHandler;
+import io.potatoBlindTest.network.ServerGame;
 import io.potatoBlindTest.network.communication.Message;
 import io.potatoBlindTest.network.communication.MessageAttachment;
 import io.potatoBlindTest.network.handlerMessage.clientNetwork.serverTypesMessages.ServerMessageType;
 import io.potatoBlindTest.network.handlerMessage.serverNetwork.ClientTypesMessages.ClientMessageHandler;
+import io.potatoBlindTest.network.handlerMessage.serverNetwork.notification.SubjectClientHandler;
 
-public class SubmitAnswer implements ClientMessageHandler<Answer> {
+import java.util.Map;
+
+public class SubmitAnswer extends SubjectClientHandler implements ClientMessageHandler<Answer> {
     @Override
     public Message handle(Answer dataMessage, ClientHandler clientHandlers) {
         System.out.println("Handling submit answer ...");
@@ -17,8 +21,59 @@ public class SubmitAnswer implements ClientMessageHandler<Answer> {
         // - if it's not    -> send FORBIDDEN code
         // - if it is       -> send OK code
         //                  -> notify all ClientHandlers with TURN_RESULT code
+        Message messageToSend = null;
+        IsCorrectAnswer isCorrectAnswer = null;
+        GameEngine serverGameEngine = ((ServerGame)clientHandlers.getServerNetwork()).getGameEngine();
         System.out.println("Server game has received the answer : " + dataMessage.getAnswer());
-        Message messageToSend = new Message(ServerMessageType.OK.getValue());
+        if (clientHandlers.getServerNetwork().isServerGame() && ((ServerGame)clientHandlers.getServerNetwork()).getStatesGame() == StatesGame.STARTED) {
+
+            isCorrectAnswer = new IsCorrectAnswer(serverGameEngine.isCorrectAnswer(dataMessage.getAnswer()));
+
+            /**
+             * Set the winner
+             */
+            if (isCorrectAnswer.isCorrect()) {
+                for (Map.Entry<Player,ClientHandler> element : ((ServerGame)clientHandlers.getServerNetwork()).getMapPlayerClientHandler().entrySet()) {
+                    if (element.getValue().equals(clientHandlers)) {
+                        serverGameEngine.getCurrentTurn().setPlayerWinner(element.getKey());
+                        break;
+                    }
+                }
+            }
+
+            messageToSend = new MessageAttachment<IsCorrectAnswer>(ServerMessageType.OK.getValue(), isCorrectAnswer);
+
+        } else {
+            messageToSend = new Message(ServerMessageType.FORBIDDEN.getValue());
+        }
+
+        if (messageToSend.getCode() == ServerMessageType.OK.getValue() && isCorrectAnswer.isCorrect()) {
+            Thread threadNotify = new Thread(() -> {
+                Message notification;
+                // Call game engine to choose a file
+                if (serverGameEngine.isLastTurn()) {
+                    TableScore tableScore = serverGameEngine.makeTableScore();
+                    notification = new MessageAttachment<TableScore>(ServerMessageType.END_GAME_RESULTS.getValue(), tableScore);
+                } else {
+                    TurnResult turnResult = new TurnResult(serverGameEngine.getCurrentTurn().getAnswer(),
+                            serverGameEngine.getCurrentTurn().getPlayerWinner().getName());
+                    notification = new MessageAttachment<TurnResult>(ServerMessageType.TURN_RESULT.getValue(), turnResult);
+                }
+
+                for (Player player: ((ServerGame)clientHandlers.getServerNetwork()).getMapPlayerClientHandler().keySet()) {
+                    try {
+                        this.notifyOne(((ServerGame)clientHandlers.getServerNetwork()).getMapPlayerClientHandler()
+                                        .get(player),
+                                notification);
+                    } catch (Exception e) {
+                        System.out.println("[ReadyPlayer]->[thread notify] Can't notify the players ...");
+                    }
+                }
+
+            });
+            threadNotify.start();
+        }
+
 
         return messageToSend;
     }
