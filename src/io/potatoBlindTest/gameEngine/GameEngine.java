c5adf3ddd42a8e1ac8e8e51e11bcab92f5e01f1f
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-10 01:47:06 +0100

    Implementation of GameEngine
    - isCorrectAnswer
    - isLastTurn
    - makeTableScore
    - ChooseFileinFolder
    - newTurn

--- src/io/potatoBlindTest/gameEngine/GameEngine.java
+++ src/io/potatoBlindTest/gameEngine/GameEngine.java
@@ -1,29 +1,68 @@
 package io.potatoBlindTest.gameEngine;
 
+import io.potatoBlindTest.network.MainServerNetwork;
+import javafx.scene.control.Tab;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class GameEngine {
     private ArrayList<Player> players;
     private Turn currentTurn;
-    private File folder;
-    private File[] filesOfFolder;
+    private File resourcesFolder;
+    private File selectedFolder;
+    private final int numberOfTurn = 3;
+    private List<File> history;
 
     public GameEngine() {
         this.players = new ArrayList<>();
         this.currentTurn = new Turn();
+        this.history = new ArrayList<>();
+        this.resourcesFolder = new File(MainServerNetwork.getDataFolderPath());
+        this.selectFolderTypeOfMedia();
+        System.out.println("[GameEngine] selected");
     }
 
     public void selectFolderTypeOfMedia() {
-        // Select randomly a folder into /home/merieme/Documents/UEJava/PotatoBlindTest
-        // TODO : replace the path if the server is elsewhere
+        File [] listFiles = resourcesFolder.listFiles();
 
+        Random rand = new Random();
+        int selected = rand.nextInt(listFiles.length);
+        this.selectedFolder = listFiles[selected];
     }
 
-    public File[] chooseFileInFolder(File folder) {
-        File[] listFiles = folder.listFiles();
+    public boolean isLastTurn() {
+        return history.size() == this.numberOfTurn;
+    }
 
-        return  listFiles;
+    public boolean isCorrectAnswer(String answer) {
+        return this.currentTurn.getAnswer().equals(answer.toUpperCase());
+    }
+
+    public Turn newTurn() {
+        File turnFile = chooseFileInFolder();
+        String answer = turnFile.getName().toUpperCase().replace("_", " ");
+        this.setCurrentTurn(new Turn(turnFile, answer));
+
+        return this.currentTurn;
+    }
+
+    public File chooseFileInFolder() {
+        File[] listFiles = this.selectedFolder.listFiles();
+        System.out.println("[GameEngine] " + listFiles);
+        Random rand = new Random();
+        int selected;
+        do {
+             selected = rand.nextInt(listFiles.length);
+        } while (this.history.contains(listFiles[selected]));
+
+        history.add(listFiles[selected]);
+
+        return listFiles[selected];
     }
 
     public ArrayList<Player> getPlayers() {
@@ -42,5 +81,25 @@ public class GameEngine {
 
     public void addPlayer(Player player) {
         players.add(player);
+    }
+
+    public TableScore makeTableScore() {
+        List<Player> players = this.players.stream().sorted((player, t1) -> t1.getScore().compareTo(player.getScore())).collect((Collectors.toList()));
+        TableScore tableScore = new TableScore(players);
+
+        int previousScore = -1;
+        int previousRank = -1;
+        for (int i = 0; i < players.size(); i ++) {
+            Player currentPlayer = players.get(i);
+            if (previousScore == currentPlayer.getScore()) {
+                currentPlayer.setRank(previousRank);
+            } else {
+                currentPlayer.setRank(i + 1);
+                previousScore = currentPlayer.getScore();
+                previousRank = i + 1;
+            }
+        }
+
+        return tableScore;
     }
 }
