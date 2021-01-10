5043af1290306428ec91d0ca1acbd320205b5efe
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-10 01:49:31 +0100

    Implementation TurnFile

--- src/io/potatoBlindTest/gameEngine/TurnFile.java
+++ src/io/potatoBlindTest/gameEngine/TurnFile.java
@@ -1,12 +1,30 @@
 package io.potatoBlindTest.gameEngine;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.Arrays;
 
 public class TurnFile implements Serializable {
     private byte[] fileByteArray;
-    public TurnFile() {
-        this.fileByteArray = new byte[]{1};
+
+    public TurnFile(File file)  {
+        try {
+            FileInputStream fileIn = new FileInputStream(file);
+
+            //find out length
+            long fileLen = file.length();
+
+            //convert length to int and create byteArray with this int
+            int intFileLen = (int) fileLen;
+            fileByteArray = new byte[intFileLen];
+            System.out.println(intFileLen);
+            //read file into byte array
+            fileIn.read(fileByteArray);
+
+            //Close FileInputStream...
+            fileIn.close();
+        } catch (IOException e) {
+            System.out.println("[TurnFile] Error while reading the file " + file.getPath());
+        }
     }
     public TurnFile(byte[] fileByteArray) {
         this.fileByteArray = fileByteArray;
