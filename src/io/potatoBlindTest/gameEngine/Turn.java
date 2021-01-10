dc8de1477accf42f32d75b76e85de75e9dab4e20
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-01-10 01:48:26 +0100

    Remove fileName in Turn

--- src/io/potatoBlindTest/gameEngine/Turn.java
+++ src/io/potatoBlindTest/gameEngine/Turn.java
@@ -5,19 +5,16 @@ import java.io.File;
 public class Turn {
 
     private Player playerWinner;
-    private String fileName;
     private File file;
     private String answer;
 
     public Turn() {
-        this.fileName = null;
         this.file = null;
         this.answer = null;
         this.playerWinner = null;
     }
 
-    public Turn(String fileName, File file, String answer) {
-        this.fileName = fileName;
+    public Turn( File file, String answer) {
         this.file = file;
         this.answer = answer;
         this.playerWinner = null;
@@ -29,6 +26,7 @@ public class Turn {
 
     public void setPlayerWinner(Player playerWinner) {
         this.playerWinner = playerWinner;
+        this.playerWinner.incrementScore();
     }
 
     public File getFile() {
@@ -51,7 +49,6 @@ public class Turn {
     public String toString() {
         return "Turn{" +
                 "playerWinner=" + playerWinner +
-                ", fileName='" + fileName + '\'' +
                 ", file=" + file +
                 ", answer='" + answer + '\'' +
                 '}';
